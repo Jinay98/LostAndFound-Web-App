0243dcc98927d81861cf5c8a547fd60426e5365c
Jinay <jinayparekh98@gmail.com>
2018-08-17 13:01:15 +0530

    Login page along with some models

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from .import views
+from django.contrib.auth.views import login,logout
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'login/$', login, {'template_name': 'accounts/login.html'}),
+
+]
