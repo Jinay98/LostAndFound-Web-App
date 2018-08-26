640d36c1bffd9ae801b702f11dc452060e7ae1d8
Jinay <jinayparekh98@gmail.com>
2018-08-26 21:36:10 +0530

    This reverts commit a3d807180d62051b3eb056829a6bb3108bfb6a47.
    ttttttt enter the commit message for your changes. Lines starting

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from accounts.models import UserProfile,ItemData
+
 # Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(ItemData)
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,5 @@ from django.contrib.auth.views import login,logout
 urlpatterns = [
     url(r'^$', views.home),
     url(r'login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'logout/$',logout,{'template_name':'accounts/logout.html'}),
-    url(r'^register/$',views.register,name='register'),
 
 ]
