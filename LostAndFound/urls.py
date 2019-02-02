e56815b03346fe102b8f6d7f493175d24e8defd9
hemal <hemalmamtora1@gmail.com>
2019-02-02 20:47:04 +0530

    Major revamp, code cleaned

--- LostAndFound/urls.py
+++ LostAndFound/urls.py
@@ -21,6 +21,6 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('',RedirectView.as_view(url='accounts/')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('accounts/', include('accounts.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,13 @@ from .import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html')),
-    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='accounts/logout.html')),
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     url(r'^register/$',views.register,name='register'),
     url(r'^profile/$',views.profile,name='profile'),
-    url(r'^request/$',views.requestItem,name='requestItem'),
-    url(r'^profile/edit$', views.editprofile, name='editprofile'),
+    url(r'^request/$',views.requestItem,name='request_item'),
+    url(r'^profile/edit$', views.editprofile, name='edit_profile'),
     url(r'^password/$', views.change_password, name='change_password'),
     url(r'^claim/(?P<id>\d+)/$', views.claim, name='claim'),
 ]
