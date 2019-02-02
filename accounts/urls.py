67bd7f769d87edd5651df6399782d13b8b2ab4d4
hemal <hemalmamtora1@gmail.com>
2019-02-02 19:38:04 +0530

    added redirect view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from .import views
-from django.contrib.auth.views import login,logout
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'logout/$', logout, {'template_name': 'accounts/logout.html'}),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html')),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='accounts/logout.html')),
     url(r'^register/$',views.register,name='register'),
     url(r'^profile/$',views.profile,name='profile'),
     url(r'^request/$',views.requestItem,name='requestItem'),
